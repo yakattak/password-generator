3f64337214e5192c05ed3a137f43db72180273fb
yakattak <stevecalebrese@gmail.com>
2022-01-03 21:08:36 -0500

    i forgot to continually commit! - added random chooser - prompts for what is required

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,58 @@
 // Assignment code here
 
 var criteria = {
-  passLength:false,
+  passLength:0,
   lowercase:false,
   uppercase:false,
   numeric:false,
   special:false,
+  reset: function() {
+    this.passLength = 0;
+    this.lowercase = false;
+    this.uppercase = false;
+    this.numeric = false;
+    this.special = false;
+  },
 }
+
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "1234567890"
+var special = "~!@#$%^&*()_+=-"
+
+var randomchooser = function (chooseFrom) {
+  var value = Math.floor(Math.random()*chooseFrom.length)
+  return value;
+};
+
+var getCriteria = function() {
+  criteria.lowercase = window.confirm("Click 'OK' to allow lowercase characters. 'Cancel' to reject.");
+  criteria.uppercase = window.confirm("Click 'OK' to allow uppercase characters. 'Cancel' to reject.");
+  criteria.numeric = window.confirm("Click 'OK' to allow numeric characters. 'Cancel' to reject.");
+  criteria.special = window.confirm("Click 'OK' to allow special characters (such as @). 'Cancel' to reject.");
+
+  while (criteria.passLength < 8 || criteria.passLength > 128 || isNaN(criteria.passLength)) {
+    criteria.passLength = window.prompt("Please enter your desired password length. (min: 8 - max: 128");
+    if (criteria.passLength === null) {
+        break;
+      } else {
+        criteria.passLength = parseInt(criteria.passLength);
+        console.log (criteria);
+      }
+}
+}
+
+var generatePassword = function() {
+  return 1234;
+
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = displayPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -24,8 +63,23 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-var generatePassword = function() {
-  window.alert("Please follow the following prompts for password critera");
-  console.log (criteria);
+var displayPassword = function() {
+  window.alert("Please follow the following prompts for password critera.");
+  getCriteria();
 
+  if (criteria.passLength === null) {
+    return "You have canceled the generator"
+  } else {
+    generatePassword();
+    criteria.reset();
+    return 1234;
+  }
+  
+  
+  
+  
 }
+
+  
+
+  
