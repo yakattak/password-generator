93eff1dbeff9ff2392f9ac383001765a9054741d
yakattak <stevecalebrese@gmail.com>
2022-01-03 21:10:51 -0500

    added comments to explain code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-// Assignment code here
-
+// set criteria for what password requires
 var criteria = {
   passLength:0,
   lowercase:false,
@@ -14,22 +13,25 @@ var criteria = {
     this.special = false;
   },
 }
-
+// create "vaults"" to get characters from
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "1234567890"
 var special = "~!@#$%^&*()_+=-"
 
+//create a random chooser function to pick a number in the length of the "vaults"
 var randomchooser = function (chooseFrom) {
-  var value = Math.floor(Math.random()*chooseFrom.length)
+  var value = Math.floor(Math.random()*chooseFrom.length);
   return value;
 };
 
+//prompt user to get the criteria from them
 var getCriteria = function() {
   criteria.lowercase = window.confirm("Click 'OK' to allow lowercase characters. 'Cancel' to reject.");
   criteria.uppercase = window.confirm("Click 'OK' to allow uppercase characters. 'Cancel' to reject.");
   criteria.numeric = window.confirm("Click 'OK' to allow numeric characters. 'Cancel' to reject.");
   criteria.special = window.confirm("Click 'OK' to allow special characters (such as @). 'Cancel' to reject.");
 
+  //repeat prompt for length until they cancel or choose an acceptable length
   while (criteria.passLength < 8 || criteria.passLength > 128 || isNaN(criteria.passLength)) {
     criteria.passLength = window.prompt("Please enter your desired password length. (min: 8 - max: 128");
     if (criteria.passLength === null) {
@@ -41,6 +43,7 @@ var getCriteria = function() {
 }
 }
 
+//generate the password
 var generatePassword = function() {
   return 1234;
 
